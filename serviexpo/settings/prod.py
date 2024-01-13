bcbc59e8082e5d07b5de62f933d3773bbb4bfb94
unknown <rammer94@gmail.com>
2024-01-13 09:13:34 -0400

    configuracion de archivo prod

--- serviexpo/settings/prod.py
+++ serviexpo/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.rammerbot.com']
 CSRF_TRUSTED_ORIGINS=['https://serviexpo.rammerbot.com']

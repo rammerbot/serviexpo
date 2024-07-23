8e42b63b3d17019a62582b00a46d75f6218b0c37
RammerBot <rammer94@gmail.com>
2024-07-23 01:00:26 -0400

    fix: prod.py

--- serviexpo/settings/prod.py
+++ serviexpo/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # Seguridad básica
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True

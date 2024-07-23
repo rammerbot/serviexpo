e17a578daa99c9b5d94aea5b0be9e70ac15b7fa6
RammerBot <rammer94@gmail.com>
2024-07-23 00:54:42 -0400

    fix: csrf prod.py

--- serviexpo/settings/prod.py
+++ serviexpo/settings/prod.py
@@ -2,9 +2,11 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+# Seguridad básica
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 ALLOWED_HOSTS = ['.rammerbot.com']
-CSRF_TRUSTED_ORIGINS=['.rammerbot.com']
+CSRF_TRUSTED_ORIGINS=['serviexpo.rammerbot.com']
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
